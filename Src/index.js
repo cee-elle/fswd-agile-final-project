6e05329b5ad4e83e6563e9f44016378b1946fff7
ruihao li / Homer <46703074+emo8355@users.noreply.github.com>
2020-03-02 22:23:52 -0800

    Update index.js

--- Src/index.js
+++ Src/index.js
@@ -3,10 +3,7 @@ frm.submit(function(e) {
   e.preventDefault();
   const url = "/api/getinfo";
   //   const url = "/get-info";
-  const food = $("#food").val();
-  const health = $("#health").val();
-  const cal = $("#calSearch").val();
-  var str = $("#searchForm :input")
+  let str = $("#searchForm :input")
     .filter(function(index, element) {
       return $(element).val() != "";
     })
@@ -16,24 +13,24 @@ frm.submit(function(e) {
     type: "post",
     data: str,
     success: function(data) {
+      $("#q").empty();
       data.forEach(x => {
         $("#q").append(
           `<div class="card" style="margin-top:30px">
               <div class="card-body">
                 <h5 class="card-title">${x.recipe.label}</h5>
                 <p class="card-text">${get_all(x.recipe.healthLabels)}</p>
+                <ul>
+                  <p>calories: ${x.recipe.calories}</p>
+                  <p>weight: ${x.recipe.totalWeight}</p>
+                </ul>
                 </div>
             </div>`
-        );
-        $("#a").append(
-          x.recipe.healthLabels.forEach(x => {
-            `<li class="list-group-item">Cras justo odio</li>`;
-          })
         );
       });
     },
     error: function(data) {
-      console.log(`error`);
+      console.log(`error,${data}`);
     }
   });
 });
