99fed4c4b232726a7f8e031e80c53e9356b65cbe
Don <donli90@hotmail.com>
2020-03-05 13:57:19 -0800

    1

--- Src/index.js
+++ Src/index.js
@@ -1,51 +1,64 @@
-const frm = $("#searchForm");
-frm.submit(function(e) {
-  e.preventDefault();
-  const url = "/api/getinfo";
-  //   const url = "/get-info";
-  const food = $("#food").val();
-  const health = $("#health").val();
-  const cal = $("#calSearch").val();
+$(function () {
 
-  $("#q").html("");
-  var str = $("#searchForm :input")
-    .filter(function(index, element) {
-      return $(element).val() != "";
-    })
-    .serialize();
+  const frm = $("#searchForm");
+  frm.submit(function (e) {
+    e.preventDefault();
+    const url = "/api/getinfo";
 
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: str,
-    success: function(data) {
-      data.forEach(x => {
-        $("#q").append(
-          `<div class="card" style="margin-top:30px">
-              <div class="card-body">
-                <h5 class="card-title">${x.recipe.label}</h5>
-                Cal: ${Math.trunc(x.recipe.calories)}
-                <p class="card-text">${get_all(x.recipe.healthLabels)}</p>
-                </div>
-            </div>`
-        );
-        // $("#a").append(
-        //   x.recipe.healthLabels.forEach(x => {
-        //     `<li class="list-group-item">Cras justo odio</li>`;
-        //   })
-        // );
-      });
-    },
-    error: function(data) {
-      console.log(`error`);
-    }
+    const food = $("#food").val();
+    const health = $("#health").val();
+    const cal = $("#calSearch").val();
+
+    console.log($("#searchForm :input"))
+
+    $("#q").html("");
+    var str = $("#searchForm :input")
+      .filter(function (index, element) {
+        return $(element).val() != "";
+      })
+      .serialize();
+    console.log(str);
+   
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: str,
+      success: function (data) {
+        data.forEach(x => {
+          console.log(x)
+          $("#q").append(
+            `<div class="card" style="margin-top:30px">
+                    <div class="card-body">
+                      <h5 class="card-title">${x.recipe.label}</h5>
+                      <p>Cal: ${Math.trunc(x.recipe.calories)}</p>
+                      <p>Portion: ${x.recipe.yield}</p>
+                      <p>Cal/Serving: ${Math.trunc(x.recipe.calories/x.recipe.yield)}</p>
+                      <p class="card-text">${get_all(x.recipe.healthLabels)}</p>
+                      </div>
+                  </div>`
+          );
+          // $("#a").append(
+          //   x.recipe.healthLabels.forEach(x => {
+          //     `<li class="list-group-item">Cras justo odio</li>`;
+          //   })
+          // );
+        });
+      },
+      error: function (data) {
+        console.log(`error`);
+      }
+    });
   });
+
+  function get_all(arr) {
+    let text = "";
+    arr.forEach(x => {
+      text += `<p class="card-text">${x}</p>`;
+    });
+    return text;
+  }
+
 });
 
-function get_all(arr) {
-  let text = "";
-  arr.forEach(x => {
-    text += `<p class="card-text">${x}</p>`;
-  });
-  return text;
-}
+
