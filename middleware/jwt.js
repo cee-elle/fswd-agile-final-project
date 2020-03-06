6773d3403eba7fa3a77bf09e55ebd50285665f53
Don <donli90@hotmail.com>
2020-03-06 08:05:44 -0800

    test

---
+++ middleware/jwt.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const secret = "hash"
+
+const checkToken = (req, res, next) => {
+  let token = req.headers['x-access-token'] || req.headers['authorization'];
+  if (token.startsWith('Bearer ')) {
+    // Remove Bearer from string
+    token = token.slice(7, token.length);
+  }
+
+  if (token) {
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err) {
+        return res.json({
+          success: false,
+          message: 'Token is not valid'
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  }
+};
+
+module.exports = {
+  checkToken: checkToken
+}
