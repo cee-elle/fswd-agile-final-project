a0894dce0d8f95556c29012e0310b4e098079079
php-fun-SkylarZhao6 <yuezezhao@gmail.com>
2020-03-28 13:19:17 -0700

    changed admin ui with bulma

--- Routes/secure_route.js
+++ Routes/secure_route.js
@@ -2,13 +2,23 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = () => {
-  router.get("/", (req, res) => {
-    const user_name = req.cookies.jwt.user.name;
-    res.render("protected", {
-      msg: `Welcome user: ${user_name}`,
-      msgClass: "alert-success"
-    });
-  });
+	router.get("/", (req, res) => {
+		const user_name = req.cookies.jwt.user.name;
+		const is_admin = req.cookies["jwt"]["user"].role;
+		if (is_admin == "admin") {
+			res.redirect("/admin/a");
+		} else if (is_admin == "premium") {
+			res.render("premium", {
+				msg: `Welcome user: ${user_name}`,
+				msgClass: "alert-success",
+			});
+		} else {
+			res.render("normal", {
+				msg: `Welcome user: ${user_name}`,
+				msgClass: "alert-success",
+			});
+		}
+	});
 
-  return router;
+	return router;
 };
